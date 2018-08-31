a25d717f6f7582b8faeb1e5c7ba6dc66d274c99a
SmartDroidies <smartdroidies@gmail.com>
2018-08-30 22:34:24 -0400

    By Naal

--- src/main/java/com/smart/droid/natkati/naal/NaalController.java
+++ src/main/java/com/smart/droid/natkati/naal/NaalController.java
@@ -1,5 +1,6 @@
 package com.smart.droid.natkati.naal;
 
+import com.sun.media.jfxmedia.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +16,13 @@ public class NaalController {
     private NaalRepository naalRepository;
 
     @GetMapping(path = "/all")
-    public @ResponseBody
-    Iterable<Naal> getAllNatkal() {
+    public @ResponseBody Iterable<Naal> getAllNatkal() {
         return naalRepository.findAll();
     }
 
     @GetMapping(path = "/naal/{id}")
-    Naal naal(@PathVariable Integer id) {
+    public @ResponseBody Naal naal(@PathVariable Integer id) {
+        Logger.logMsg(Logger.INFO, "Path Variable : " + id);
         return naalRepository.findById(id)
                 .orElseThrow(() -> new NaalNotFoundException(id));
     }
