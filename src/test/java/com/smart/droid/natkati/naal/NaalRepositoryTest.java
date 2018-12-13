20d7560cf9aa86130b08a97112ab6a2a1df8320d
SmartDroidies <smartdroidies@gmail.com>
2018-12-13 07:55:55 -0500

    Repository test case

---
+++ src/test/java/com/smart/droid/natkati/naal/NaalRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.smart.droid.natkati.naal;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class NaalRepositoryTest {
+
+}
