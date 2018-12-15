1597447ce28dbca48bbab0bcaeed8e3aa58fabc1
SmartDroidies <smartdroidies@gmail.com>
2018-12-14 20:52:31 -0500

    Repository test case

--- src/main/java/com/smart/droid/natkati/naal/NaalRepository.java
+++ src/main/java/com/smart/droid/natkati/naal/NaalRepository.java
@@ -1,7 +1,12 @@
 package com.smart.droid.natkati.naal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
+@Repository
 public interface NaalRepository extends JpaRepository<Naal, Integer> {
 
+    Naal findByCalDate(LocalDate calDate);
 }
--- src/test/java/com/smart/droid/natkati/naal/NaalRepositoryTest.java
+++ src/test/java/com/smart/droid/natkati/naal/NaalRepositoryTest.java
@@ -1,11 +1,67 @@
 package com.smart.droid.natkati.naal;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class NaalRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private NaalRepository naalRepository;
+
+
+    @Test
+    public void findById() {
+        Naal naalTest = createNaalInstanse();
+        entityManager.persist(naalTest);
+        entityManager.flush();
+
+        Optional<Naal> found = naalRepository.findById(2019001);
+        assertThat(found.get().getCalDate()).isEqualTo(LocalDate.of(2019,1,1));
+    }
+
+    @Test
+    public void findByNaal() {
+        Naal naalTest = createNaalInstanse();
+        entityManager.persist(naalTest);
+        entityManager.flush();
+
+        Naal found = naalRepository.findByCalDate(LocalDate.of(2019,1,1));
+        assertThat(found.getCalDate()).isEqualTo(LocalDate.of(2019,1,1));
+    }
+
+    @Test
+    public void retriveAllRecords() {
+        Naal naalTest = createNaalInstanse();
+        entityManager.persist(naalTest);
+        entityManager.flush();
+
+        List<Naal> all = naalRepository.findAll();
+        assertThat(all.size()).isEqualTo(1);
+    }
+
+    private Naal createNaalInstanse() {
+        Naal naal = new Naal();
+        naal.setCalDate(LocalDate.of(2019,1,1));
+
+        naal.setId(2019001);
+
+        return naal;
+    }
+
 
 }
